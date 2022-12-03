9e286baa1563bf71363af95c41fd5159448baf29
Lautaro Garcia <lautarojgarcia177@gmail.com>
2022-12-03 10:48:51 -0300

    ES9

--- 2__Nuevas-funcionalidades-ECMAScript/index.js
+++ 2__Nuevas-funcionalidades-ECMAScript/index.js
@@ -1,8 +1,10 @@
 // ES7
+console.log("--------------------", "ES7", "--------------------");
 console.log("Exponencial:", 3 ** 3);
 console.log("Array includes:", [1, 2, 3].includes(1));
 
 // ES8
+console.log("--------------------", "ES8", "--------------------");
 const foo = {
   nombre: "Lautaro",
   edad: 27,
@@ -12,6 +14,7 @@ console.log("object.values:", Object.values(foo));
 console.log("object.keys:", Object.keys(foo));
 
 // ES9
+console.log("--------------------", "ES9", "--------------------");
 let sum;
 try {
   throw "ERROR!";
@@ -34,5 +37,29 @@ const numbers = [1, 2, 3];
   console.log("spread syntax:", x + y + z);
 })(...numbers);
 console.log("spread syntax 2:", [0, ...numbers]);
+console.log("spread syntax 3:", { ...numbers });
+const dogObject = {
+  patas: 4,
+  ojos: 2,
+};
+console.log("spread syntax 4:", { ...dogObject, raza: "rottweiler", edad: 10 });
 
 // ES10
+console.log("--------------------", "ES10", "--------------------");
+console.log("string trim:", "     Trimmed string      ".trim());
+console.log("array flat:", [0, 1, 2, [3, 4]].flat());
+
+// ES11
+console.log("--------------------", "ES11", "--------------------");
+console.log(false || "Valor por defecto");
+console.log("nullish:", false ?? "Valor por defecto");
+console.log("nullish:", null ?? "Valor por defecto");
+
+class ClaseConCampoPrivado {
+  static #campoPrivado = "este es un campo privado de la clase";
+  static get _campoPrivado() {
+    return this.#campoPrivado;
+  }
+}
+// console.log(ClaseConCampoPrivado.#campoPrivado);
+console.log(ClaseConCampoPrivado._campoPrivado);
