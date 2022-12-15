faeca0ef81a92caf4c8e0d1763243cf513b5fe34
Lautaro Garcia <lautarojgarcia177@gmail.com>
2022-12-15 19:30:12 -0300

    Hands on lab calculadora

--- 3_Programacion-Asincrona/hands_on_lab__Calculadora.js
+++ 3_Programacion-Asincrona/hands_on_lab__Calculadora.js
@@ -1,24 +1,60 @@
 function suma(sumando1, sumando2) {
   return new Promise(function (resolve, reject) {
-    // TODO completar
+    if (sumando1 === 0 || sumando2 === 0) {
+      return reject("No se puede sumar 0");
+    }
+    let resultado = sumando1 + sumando2;
+    if (resultado <= 0) {
+      return reject("La calculadora solo debe devolver valores positivos");
+    }
+    return resolve(resultado);
   });
 }
 
 const resta = (minuendo, sustraendo) =>
   new Promise((resolve, reject) => {
-    // TODO completar
-    resolve(10);
+    if (minuendo === 0 || sustraendo === 0) {
+      return reject("Operacion invalida");
+    }
+    let resultado = minuendo - sustraendo;
+    if (resultado <= 0) {
+      return reject("La calculadora solo debe devolver valores positivos");
+    }
+    resolve(resultado);
   });
 
 // Multiplicacion
+const multiplicacion = (numero1, numero2) =>
+  new Promise((resolve, reject) => {
+    if (numero1 <= 0 || numero2 <= 0) {
+      reject("Ninguno de los valores puede ser negativo");
+    }
+    let resultado = numero1 * numero2;
+    resolve(resultado);
+  });
 
 // Division
+function division(dividendo, divisor) {
+  return new Promise(function (resolve, reject) {
+    if (divisor === 0) {
+      reject("No se puede divir por 0");
+    } else {
+      let resultado = dividendo / divisor;
+      if (resultado <= 0) {
+        return reject("La calculadora solo debe devolver valores positivos");
+      }
+      return resolve(resultado);
+    }
+  });
+}
 
-async function calculos () {
+async function calculos() {
+  let res;
   try {
-    // TODO Hacer pruebas con await
+    // res = await multiplicacion(1, 2);
+    console.log(res);
   } catch (error) {
-    // TODO Handlear errores
+    throw new Error(error);
   }
 }
 
