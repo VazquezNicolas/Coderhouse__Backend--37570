e9e2df1af875cc95e461453f228c260005f43c2a
Lautaro Garcia <lautarojgarcia177@gmail.com>
2022-12-21 17:29:01 -0300

    nodemon

--- 5_npm/actividad_en_clase.js
+++ 5_npm/actividad_en_clase.js
@@ -2,7 +2,7 @@ const obj = {};
 
 function generarNumeros() {
   for (let i = 0; i < 10000; i++) {
-    let numero = Math.round(Math.random() * (20 - 1));
+    let numero = Math.round(Math.random() * (20 + 1));
     if (!obj.hasOwnProperty(numero)) {
       obj[numero] = 1;
     } else {
