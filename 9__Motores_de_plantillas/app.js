bad899971f27f59af851c45576d2b64267b83fe0
Lautaro Garcia <lautarojgarcia177@gmail.com>
2023-01-10 20:53:15 -0300

    Motores de plantillas

--- 9__Motores_de_plantillas/app.js
+++ 9__Motores_de_plantillas/app.js
@@ -3,16 +3,42 @@ const handlebars = require("express-handlebars");
 
 const app = express();
 
-app.engine('handlebars', handlebars.engine());
-app.set('view engine', 'handlebars');
-app.set('views', __dirname + '/views');
+app.engine("handlebars", handlebars.engine());
+app.set("view engine", "handlebars");
+app.set("views", __dirname + "/views");
 
+app.use(express.static(__dirname + "/public"));
+
+const users = [
+  {
+    nombre: "Elba",
+    apellido: "Surero",
+    correo: "elba_surero@gmail.com",
+    telefonos: [
+      {
+        numero: "08009990271",
+      },
+      {
+        numero: "+54935134466775",
+      },
+    ],
+    esMayor: true,
+  },
+  {
+    nombre: "Aitor",
+    apellido: "Tilla",
+    correo: "aitor_tilla@gmail.com",
+    telefonos: "08009990272",
+    esMayor: false,
+  },
+];
 app.get("/", (req, res) => {
-    res.render('home')
+  const random = Number(Math.random().toFixed());
+  res.render("datos_personales", users[random]);
 });
 app.get("/saludar/:nombre", (req, res) => {
-    const nombre = req.params.nombre;
-    res.render('saludar', { nombre });
+  const nombre = req.params.nombre;
+  res.render("saludar", { nombre, stylesheet: 'nombre' });
 });
 
 const PORT = 3000;
